ebaf27bf918d253de7d4e856cbffd9738744dec8
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-12-25 15:26:07 +0200

    update

--- 09. Length of Last Name.sql
+++ 09. Length of Last Name.sql
@@ -1,1 +1,3 @@
-﻿
+﻿SELECT FirstName,LastName
+FROM Employees
+WHERE LEN(LastName)=5
--- 10. Rank Employees by Salary.sql
+++ 10. Rank Employees by Salary.sql
@@ -1,1 +1,4 @@
-﻿
+﻿SELECT EmployeeID,FirstName,LastName,Salary,DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID)
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
