dc626120a0d109b57fa3d496e13eb42148d0b962
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-12-27 15:22:34 +0200

    update

--- 06. Employees Hired After.sql
+++ 06. Employees Hired After.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT e.FirstName,e.LastName,e.HireDate,d.Name AS DeptName
+FROM [Employees] AS e
+JOIN Departments d ON e.DepartmentID=d.DepartmentID
+WHERE HireDate> '1999-01-01' AND d.Name='Sales' OR d.Name='Finance'
+ORDER BY e.HireDate
--- 07. Employees With Project.sql
+++ 07. Employees With Project.sql
@@ -1,1 +1,6 @@
-﻿
+﻿SELECT TOP(5)e.EmployeeID,e.FirstName,p.Name
+ FROM Employees AS e
+ JOIN EmployeesProjects AS ep ON ep.EmployeeID=e.EmployeeID
+ JOIN Projects AS p ON p.ProjectID=ep.ProjectID
+ WHERE p.StartDate> '08/13/2002' AND p.EndDate IS NULL
+ ORDER BY e.EmployeeID
