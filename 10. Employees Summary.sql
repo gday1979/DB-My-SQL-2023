924ab498c1fd42dca0edea432abb9efd92d16c1e
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-12-28 12:29:32 +0200

    update

--- 10. Employees Summary.sql
+++ 10. Employees Summary.sql
@@ -1,1 +1,6 @@
-﻿
+﻿SELECT TOP(50) emp.EmployeeID,emp.FirstName+ ' ' +emp.LastName,
+mng.FirstName+ ' ' +mng.LastName,d.Name
+FROM Employees AS emp
+JOIN Employees AS mng ON mng.EmployeeID=emp.ManagerID
+JOIN Departments AS d ON d.DepartmentID=emp.DepartmentID
+ORDER BY EmployeeID
