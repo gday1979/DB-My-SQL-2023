28f9e74ffbbf330f66a512ee9ed19ce8964547b7
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-01 13:29:31 +0200

    update

--- 04. Employees from Town.sql
+++ 04. Employees from Town.sql
@@ -1,1 +1,7 @@
-﻿
+﻿CREATE PROC usp_GetEmployeesFromTown (@townName NVARCHAR(50))
+AS
+SELECT FirstName,LastName
+  FROM Employees AS e
+  JOIN Addresses AS a ON a.AddressID=e.AddressID
+  JOIN Towns AS t ON t.TownID=a.TownID
+  WHERE t.Name=@townName
