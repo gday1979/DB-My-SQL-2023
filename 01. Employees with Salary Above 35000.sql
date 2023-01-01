f500f2033970cc70b379f1c68470270d4580a1cc
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-01 10:52:17 +0200

    update

---
+++ 01. Employees with Salary Above 35000.sql
@@ -0,0 +1,5 @@
+CREATE PROC usp_GetEmployeesSalaryAbove35000 
+  AS
+  SELECT FirstName,LastName
+  FROM Employees
+  WHERE Salary >35000
--- 02. Employees with Salary Above Number.sql
+++ 02. Employees with Salary Above Number.sql
@@ -1,1 +1,5 @@
-﻿
+﻿CREATE PROC usp_GetEmployeesSalaryAboveNumber (@inputSalary DECIMAL(18,4))
+AS
+SELECT FirstName,LastName
+  FROM Employees
+  WHERE Salary >=@inputSalary
