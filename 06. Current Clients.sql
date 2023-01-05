edb3a45615b6d8aa34ec3e8d17b5103c8c2f21c5
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-05 18:53:27 +0200

    update

--- 06. Current Clients.sql
+++ 06. Current Clients.sql
@@ -1,1 +1,8 @@
-﻿
+﻿SELECT 
+CONCAT(c.FirstName,' ',c.LastName) AS Client
+,DATEDIFF(DAY, j.IssueDate, '2017-04-24') AS [Days going]
+,j.[Status]
+FROM Jobs AS j
+JOIN Clients AS c ON j.ClientId = c.ClientId
+WHERE j.[Status] != 'Finished'
+ORDER BY [Days going] DESC, c.ClientId
