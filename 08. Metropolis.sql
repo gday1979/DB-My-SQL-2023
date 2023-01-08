561077f659b916835534502005b5ac562a0d089f
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-08 10:41:45 +0200

    update

--- 08. Metropolis.sql
+++ 08. Metropolis.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT TOP(10) c.Id, c.Name,c.CountryCode, COUNT(*) AS Accounts
+FROM Accounts a
+JOIN Cities c ON a.CityId=c.Id
+GROUP BY c.Id,c.Name, c.CountryCode
+ORDER BY Accounts DESC
--- 09. Romantic Getaways.sql
+++ 09. Romantic Getaways.sql
@@ -1,1 +1,11 @@
-﻿
+﻿SELECT AccountId AS Id,Email,ac.Name AS City,COUNT(*) AS Trips
+  FROM AccountsTrips at
+  JOIN Accounts a ON at.AccountId=a.Id
+  JOIN Cities ac ON a.CityId=ac.Id
+  JOIN Trips t ON at.TripId=t.Id
+  JOIN Rooms r ON t.RoomId=r.Id
+  JOIN Hotels h ON r.HotelId=h.Id
+  JOIN Cities hc ON h.CityId=hc.Id
+  WHERE hc.Id=ac.Id
+  GROUP BY AccountId,Email,ac.Name
+  ORDER By Trips DESC ,AccountId ASC
