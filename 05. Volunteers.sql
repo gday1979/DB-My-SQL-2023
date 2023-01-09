1ae0b4611f3149aea2a9bc0251028f98e0920db5
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-09 18:57:55 +0200

    update

--- 05. Volunteers.sql
+++ 05. Volunteers.sql
@@ -1,1 +1,3 @@
-﻿
+﻿SELECT Name,PhoneNumber,Address,AnimalId,DepartmentId
+FROM Volunteers
+ORDER BY Name ASC,AnimalId ASC,DepartmentId ASC
--- 06. Animals data.sql
+++ 06. Animals data.sql
@@ -1,1 +1,4 @@
-﻿
+﻿SELECT a.Name,AnimalType,FORMAT(Birthdate,'dd.MM.yyyy') AS BirthDate
+FROM Animals AS a
+JOIN AnimalTypes as T ON a.AnimalTypeId=T.Id
+ORDER BY a.Name ASC
