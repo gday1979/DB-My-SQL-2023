bb1f4e95b47467a6e64f3c8f1b997fcf6157f15b
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-12 20:44:12 +0200

    update

--- 04.Delete 19 June 2022.sql
+++ 04.Delete 19 June 2022.sql
@@ -1,1 +1,7 @@
-﻿
+﻿DELETE FROM Volunteers
+WHERE DepartmentId IN (SELECT Id 
+						FROM VolunteersDepartments 
+						WHERE DepartmentName = 'Education program assistant')
+
+DELETE FROM VolunteersDepartments 
+WHERE DepartmentName = 'Education program assistant'
--- 14. Car Rental Database.sql
+++ 14. Car Rental Database.sql
@@ -1,4 +1,4 @@
-﻿CREATE DATABASE CarRental
+﻿
 
 CREATE TABLE Categories
 (
