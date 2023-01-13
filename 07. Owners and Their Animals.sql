e4cd237d65040d0206f396a9b7562f210748cfdf
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-13 18:58:39 +0200

    update

--- 07. Owners and Their Animals.sql
+++ 07. Owners and Their Animals.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT TOP(5) o.Name AS Owner,COUNT(a.Id) AS CountAnimals
+FROM Animals AS a
+  JOIN Owners AS o ON a.OwnerId=o.Id
+  GROUP BY o.Name
+  ORDER BY COUNT( a.Id) DESC,o.Name
--- 08. Owners, Animals and Cages.sql
+++ 08. Owners, Animals and Cages.sql
@@ -1,1 +1,11 @@
 ﻿
+SELECT 
+ CONCAT(O.[Name], '-', A.[Name]) AS OwnersAnimals
+,O.PhoneNumber
+,C.CageId
+FROM Animals AS A
+JOIN Owners AS O ON A.OwnerId = O.Id
+JOIN AnimalsCages AS C ON A.Id = C.AnimalId
+JOIN AnimalTypes AS T ON A.AnimalTypeId = T.Id
+WHERE T.AnimalType = 'Mammals'
+ORDER BY O.[Name], A.[Name] DESC
