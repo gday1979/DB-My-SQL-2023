b3203240fadc3533b07e1ccbbf637d6cf15cd91f
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-20 14:27:05 +0200

    update

--- 11. Client with Cigars.sql
+++ 11. Client with Cigars.sql
@@ -1,1 +1,15 @@
-﻿
+﻿REATE FUNCTION udf_ClientWithCigars(@name NVARCHAR(MAX))
+RETURNS INT
+AS
+BEGIN
+             DECLARE @countOfCigars INT;
+			 SET @countOfCigars=
+			 (SELECT
+			       COUNT(*)
+				   FROM Clients cl
+				   JOIN ClientsCigars cc ON cc.ClientId=cl.Id
+				   JOIN Cigars c ON cc.CigarId=c.Id
+				   WHERE cl.FirstName LIKE @name)
+				   RETURN @countOfCigars
+
+END
--- 12. Search for Cigar with Specific Taste.sql
+++ 12. Search for Cigar with Specific Taste.sql
@@ -1,1 +1,15 @@
-﻿
+﻿CREATE PROCEDURE usp_SearchByTaste(@taste VARCHAR(30))
+AS
+	SELECT
+	c.CigarName
+	,CONCAT('$',c.PriceForSingleCigar) AS Price
+	,t.TasteType
+	,b.BrandName
+	,CONCAT(s.[Length], ' cm') AS CigarLength
+	,CONCAT(s.RingRange, ' cm') AS CigarRingRange
+	FROM Cigars AS c
+	JOIN Tastes AS t ON c.TastId = t.Id
+	JOIN Brands AS b ON c.BrandId = b.Id
+	JOIN Sizes AS s ON c.SizeId = s.Id
+	WHERE t.TasteType = @taste
+	ORDER BY s.[Length], s.RingRange DESC
