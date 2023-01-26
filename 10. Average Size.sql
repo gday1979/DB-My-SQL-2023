da21443065bc9d17cdc8f5161606e9eb40044215
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-26 15:58:49 +0200

    commit

--- 10. Average Size.sql
+++ 10. Average Size.sql
@@ -1,1 +1,6 @@
-﻿
+﻿SELECT u.UserName,AVG(f.Size) AS Size
+FROM Users AS u
+JOIN Commits c ON u.Id=c.ContributorId
+JOIN Files f ON c.Id=f.CommitId
+GROUP BY u.Id,u.UserName
+ORDER BY Size DESC,u.UserName
--- 11. All User Commits.sql
+++ 11. All User Commits.sql
@@ -1,1 +1,11 @@
-﻿
+﻿CREATE FUNCTION udf_AllUserCommits(@username VARCHAR(30)) 
+RETURNS INT
+AS
+BEGIN
+      RETURN
+	   (SELECT
+	          COUNT(c.Id)
+			  FROM Commits As c
+			  JOIN Users AS u ON c.ContributorId=u.Id
+			  WHERE u.UserName=@username)
+END
