7161f35bc6e00ba5219f6fde225e93b7cf5a015a
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-01-30 18:22:39 +0200

    update

--- 06. Reports  Categories.sql
+++ 06. Reports  Categories.sql
@@ -1,1 +1,4 @@
-﻿
+﻿SELECT r.Description,c.Name AS CategoryName
+FROM Reports AS r
+JOIN Categories AS c ON r.CategoryId=c.Id
+ORDER BY r.Description,c.Name
--- 07. Most Reported Category.sql
+++ 07. Most Reported Category.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT TOP(5)c.Name AS CategoryName,COUNT(r.Id) ReportsNumber
+FROM Reports AS r
+LEFT JOIN Categories AS c ON r.CategoryId=c.Id
+GROUP BY c.Name
+ORDER BY ReportsNumber DESC,CategoryName
