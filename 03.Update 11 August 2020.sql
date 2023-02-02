c46f77b32ceddcf17311595ba1d6e2ce60814056
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-02 18:49:11 +0200

    update

--- 03.Update 11 August 2020.sql
+++ 03.Update 11 August 2020.sql
@@ -1,1 +1,8 @@
-﻿
+﻿UPDATE Ingredients
+SET DistributorId = 35
+WHERE [Name] = 'Bay Leaf'
+	OR [Name] = 'Paprika'
+	OR [Name] = 'Poppy'
+UPDATE Ingredients
+SET OriginCountryId = 14
+WHERE OriginCountryId = 8
