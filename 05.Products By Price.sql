1f291716d7b7fe21543e2e8183f69de4f7287d34
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-02 20:47:35 +0200

    update

--- 05.Products By Price.sql
+++ 05.Products By Price.sql
@@ -1,1 +1,3 @@
-﻿
+﻿SELECT p.Name,P.Price,P.Description
+FROM Products AS p
+ORDER BY p.Price DESC,p.Name ASC
--- 06.Negative Feedback.sql
+++ 06.Negative Feedback.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT f.ProductId,f.Rate,f.Description,f.CustomerId,c.Age,c.Gender
+FROM Feedbacks AS F
+JOIN Customers AS C ON f.CustomerId=c.Id
+WHERE f.Rate<5.0
+ORDER BY f.ProductId DESC,f.Rate ASC
