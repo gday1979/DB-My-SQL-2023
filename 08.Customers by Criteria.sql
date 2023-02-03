a3c23b8361a3b714c29bb04ac93045bbb3c4da5a
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-03 21:03:03 +0200

    update

--- 08.Customers by Criteria.sql
+++ 08.Customers by Criteria.sql
@@ -1,1 +1,6 @@
-﻿
+﻿SELECT c.FirstName,c.Age,c.PhoneNumber
+FROM Customers AS c
+JOIN Countries AS cs ON c.CountryId=cs.Id
+WHERE (c.Age >=21 AND c.FirstName LIKE '%an%')
+       OR( c.PhoneNumber LIKE '%38' AND cs.Name != 'Greece')
+ORDER BY c.FirstName,c.Age DESC
