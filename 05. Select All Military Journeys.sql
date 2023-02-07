baa094eed3041b34af32cec363bf888bd0af78e5
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-07 20:40:31 +0200

    update

--- 05. Select All Military Journeys.sql
+++ 05. Select All Military Journeys.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT j.id,FORMAT(j.JourneyStart,'dd/MM/yyyy') AS JourneyStart
+,FORMAT(j.JourneyEnd,'dd/MM/yyyy')AS JourneyEnd
+FROM Journeys AS j
+WHERE Purpose='Military'
+ORDER BY j.JourneyStart 
