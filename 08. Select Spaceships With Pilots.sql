d17197398b82d023b2d4c184a0f48112d2f12fee
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-08 20:42:39 +0200

    update

--- 08. Select Spaceships With Pilots.sql
+++ 08. Select Spaceships With Pilots.sql
@@ -1,1 +1,8 @@
-﻿
+﻿SELECT s.Name,s.Manufacturer
+FROM Spaceships AS s
+JOIN Journeys AS j ON j.SpaceshipId=s.Id
+JOIN TravelCards AS tc ON tc.JourneyId=j.Id
+JOIN Colonists AS c ON tc.ColonistId=c.Id
+WHERE c.BirthDate>'01/01/1989' AND tc.JobDuringJourney='Pilot'
+ORDER BY s.Name
+
