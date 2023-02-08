67db01fffec56169f47f7ea390bc457f5ca65db9
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-08 19:19:57 +0200

    update

--- 06. Select All Pilots.sql
+++ 06. Select All Pilots.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT c.Id,c.FirstName+ ' ' +c.LastName AS FullName
+FROM Colonists AS c
+JOIN TravelCards AS tc ON tc.ColonistId=c.Id
+WHERE tc.JobDuringJourney='Pilot'
+ORDER BY c.Id
--- 07. Count Colonists.sql
+++ 07. Count Colonists.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT COUNT(j.Purpose) AS Count
+FROM Colonists AS c
+JOIN TravelCards AS tc ON tc.ColonistId=c.Id
+JOIN Journeys AS j ON tc.JourneyId=j.Id
+WHERE j.Purpose='Technical'
