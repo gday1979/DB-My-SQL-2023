321dd86cca1644150148d36ea05417fbec084d3b
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-09 18:59:36 +0200

    update

--- 10. Select Special Colonists.sql
+++ 10. Select Special Colonists.sql
@@ -1,1 +1,9 @@
-﻿
+﻿SELECT JobDuringJourney,FullName,JobRank
+FROM (
+        SELECT tc.JobDuringJourney,
+		c.FirstName+ ' ' +c.LastName AS FullName,
+		RANK() OVER(PARTITION BY JobDuringJourney ORDER BY BirthDate ASC) AS JobRank
+		FROM Colonists AS c
+		JOIN TravelCards AS tc ON c.Id=tc.ColonistId
+)AS ranked
+WHERE JobRank=2
