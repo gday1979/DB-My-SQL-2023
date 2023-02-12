89335ae0078853e88595770f616a3d1ac1728d19
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-12 15:19:49 +0200

    update

--- 02. All Users in Games.sql
+++ 02. All Users in Games.sql
@@ -1,1 +1,8 @@
-﻿
+﻿SELECT g.Name AS Game,gp.Name AS GameType,u.Username,ug.Level,ug.Cash,c.Name AS Character
+FROM Games AS g
+JOIN GameTypes AS gp ON g.GameTypeId=gp.Id
+JOIN UsersGames AS ug ON g.Id=ug.GameId
+JOIN Users AS u ON ug.UserId=u.Id
+JOIN Characters AS c ON ug.CharacterId=c.Id
+ORDER BY Level DESC,Username ASC,Game
+
