1f316b30b22fd8e38b664963ac6d134583196350
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-02-12 11:16:46 +0200

    update

--- 11. Get Colonists Count.sql
+++ 11. Get Colonists Count.sql
@@ -1,1 +1,15 @@
-﻿
+﻿CREATE FUNCTION dbo.udf_GetColonistsCount
+(@PlanetName VARCHAR (30)) 
+RETURNS INT
+AS
+BEGIN
+RETURN
+(
+    SELECT COUNT(*)
+	  FROM Planets AS p
+	   JOIN Spaceports AS s ON p.Id=s.PlanetId
+	  JOIN Journeys AS j ON s.Id=j.DestinationSpaceportId
+	  JOIN TravelCards AS tc ON j.Id=tc.JourneyId
+	  WHERE p.Name=@PlanetName
+	  )
+END
